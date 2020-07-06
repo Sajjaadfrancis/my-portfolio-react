97b7ef5824d4cbc52737af61526b80db6eb95fa9
unknown <sajjaadfrancis@gmail.com>
2020-07-06 21:48:18 +0200

    Updates

---
+++ precache-manifest.ca90ce75bd470ba20ea00574b60bfbdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7250904d20bb9a736f6c39c2c48b75b",
+    "url": "/my-portfolio-react/index.html"
+  },
+  {
+    "revision": "efafbc25f4d6afdeeb74",
+    "url": "/my-portfolio-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cad42e1f519a0d1eec83",
+    "url": "/my-portfolio-react/static/js/2.be3225b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio-react/static/js/2.be3225b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efafbc25f4d6afdeeb74",
+    "url": "/my-portfolio-react/static/js/main.7556566b.chunk.js"
+  },
+  {
+    "revision": "0a5b6a8f947236c0bef5",
+    "url": "/my-portfolio-react/static/js/runtime-main.402e02b4.js"
+  }
+]);
